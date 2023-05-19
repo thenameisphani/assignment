1bc7aeeaa7640b828472ebf2fe83cc977cfa7461
thenameisphani <109454268+thenameisphani@users.noreply.github.com>
2023-05-19 16:05:11 +0530

    Add files via upload

---
+++ move.js
@@ -0,0 +1,44 @@
+// getting elements from html and storing it into variable
+
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// fetch currency rates and update dom
+
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(
+    `https://v6.exchangerate-api.com/v6/2838b1edd298b25b40aaf0ef/latest/${currency_one}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      // accessing conversion rates from api
+
+      const rate = data.conversion_rates[currency_two];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+}
+
+// event listeners
+currencyEl_one.addEventListener('change', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+amountEl_two.addEventListener('input', calculate);
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
+
+// calling the function
+calculate();
